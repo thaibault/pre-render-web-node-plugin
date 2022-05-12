08898bc742f02e323bc9b43f0c1b832dabcf5ee3
torben <t.sickert[~at~]gmail.com>
2022-05-12 21:17:47 +0200

    Automatic update to new version.

--- test.ts
+++ test.ts
@@ -14,6 +14,7 @@
     endregion
 */
 // region imports
+import {beforeAll, describe, expect, test} from '@jest/globals'
 import Tools from 'clientnode'
 import {RecursivePartial} from 'clientnode/type'
 import {configuration as baseConfiguration, PluginAPI} from 'web-node'
