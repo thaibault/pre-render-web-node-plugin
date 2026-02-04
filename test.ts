b6c59a9aaa266ea90b4053146e34354e85359591
thaibault <info@torben.website>
2026-02-04 18:24:31 +0100

    Automatic update to new version.

--- test.ts
+++ test.ts
@@ -15,11 +15,21 @@
 */
 // region imports
 import {beforeAll, describe, expect, test} from '@jest/globals'
-import {copy, extend, RecursivePartial} from 'clientnode'
+import {copy, extend, RecursivePartial, timeout} from 'clientnode'
+import childProcess from 'child_process'
 import {configuration as baseConfiguration, loadAll, pluginAPI} from 'web-node'
+import {ServicePromises} from 'web-node/type'
 
-import {postConfigurationHotLoaded} from './index'
-import {Configuration} from './type'
+import {
+    getPrerenderedOutputDirectories,
+    getPrerendererExecuter,
+    postConfigurationHotLoaded,
+    preLoadService,
+    render,
+    renderFile,
+    shouldExit
+} from './index'
+import {Configuration, Services} from './type'
 // endregion
 describe('preRender', (): void => {
     // region mockup
@@ -47,5 +57,76 @@ describe('preRender', (): void => {
             pluginAPI
         })).resolves.toBeUndefined()
     })
-    // TODO test all methods
+    test('preLoadService', async (): Promise<void> => {
+        const testConfiguration: Configuration = extend<Configuration>(
+            true,
+            copy(configuration),
+            {preRender: {renderAfterConfigurationUpdates: false}} as
+                RecursivePartial<Configuration>
+        )
+        const state = {
+            configuration: testConfiguration,
+            data: undefined,
+            hook: 'preLoadService',
+            plugins: [],
+            services: {} as Services,
+            pluginAPI
+        }
+
+        await expect(preLoadService(state)).resolves.toBeUndefined()
+        expect(state).toHaveProperty('services.preRender.renderFile')
+    })
+    test('shouldExit', async (): Promise<void> => {
+        const state = {
+            configuration,
+            data: undefined,
+            hook: 'shouldExit',
+            plugins: [],
+            servicePromises: {} as ServicePromises,
+            services: {preRender: {}} as Services,
+            pluginAPI
+        }
+
+        await expect(shouldExit(state)).resolves.toBeUndefined()
+
+        state.services.preRender.getPrerenderedOutputDirectories =
+            () => Promise.resolve([])
+
+        await expect(shouldExit(state)).resolves.toBeUndefined()
+    })
+    test('getPrerenderedOutputDirectories', async (): Promise<void> => {
+        await expect(getPrerenderedOutputDirectories(
+            configuration, [], pluginAPI
+        )).resolves.toStrictEqual([])
+    })
+    test('getPrerendererExecuter', async (): Promise<void> => {
+        await expect(getPrerendererExecuter(
+            configuration, [], pluginAPI
+        )).resolves.toStrictEqual([])
+    })
+    test('render', async (): Promise<void> => {
+        await expect(render({
+            configuration,
+            data: undefined,
+            hook: 'shouldExit',
+            plugins: [],
+            servicePromises: {} as ServicePromises,
+            services: {preRender: {}} as Services,
+            pluginAPI
+        })).resolves.toBeUndefined()
+    })
+    test('renderFile', async (): Promise<void> => {
+        const closeEventHandler = ((
+            _eventName: string, callback: () => void
+        ) => {
+            void timeout(callback)
+        })
+        childProcess.spawn = (() => ({on: closeEventHandler})) as
+            unknown as
+            typeof childProcess['spawn']
+
+        await expect(renderFile('', [])).resolves.toStrictEqual(
+            {parameters: [], reason: null}
+        )
+    })
 })
